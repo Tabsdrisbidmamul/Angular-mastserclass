b3a9db4a1313f5a11f9c6e819522db80eca357fd
Idris <idris@greenwoodcampbell.com>
2022-02-08 17:30:42 +0000

    Added basic routing

---
+++ section11-routing/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../../services/servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css'],
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number; name: string; status: string } | undefined;
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serversService: ServersService) {}
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server!.name;
+    this.serverStatus = this.server!.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server!.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
+  }
+}
